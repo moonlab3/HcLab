be85d69b4220fb4cdeddda1c642af91b5755e147
H스타트업팀 <h-startupteam@Hseutateueobtim-ui-MacBookPro.local>
2022-12-15 10:50:50 +0900

    modified http -> https

--- redirect.js
+++ redirect.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
-
-const http = require('http');
+const fs = require('fs');
 const { stringify } = require('querystring');
-const server = http.createServer(app);
 
 const port = 3002;
 
+const https = require('https');
+const options = {
+  key: fs.readFileSync(__dirname + '/selfsigned.key'),
+  cert: fs.readFileSync(__dirname + '/selfsigned.crt')
+}
+const server = https.createServer(options, app) ;
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
 app.use(cookieParser());
+app.use(express.json());
 
 app.get('/checkcookie', (req, res) => {
   if(req.headers.cookie !== undefined) {
@@ -19,12 +29,9 @@ app.get('/checkcookie', (req, res) => {
   }
   console.log('req.cookie:' + JSON.stringify(req.cookies));
 });
+
 app.get('/', (req, res) => {
   console.log('header:' + JSON.stringify(req.headers));
   res.cookie('cookieOne', 'minkyu');
   res.sendFile(__dirname + '/indexCookie.html');
-});
-
-server.listen(port, function (req, res) {
-  console.log(`listening on ${port}`);
 });
